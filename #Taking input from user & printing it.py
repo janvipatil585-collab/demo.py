79059f9544403d6786eb2139a2f0b15678c7bb15
janvipatil585-collab <janvipatil585@gmail.com>
2025-12-15 22:07:21 +0530

    Add files via upload

---
+++ #Taking input from user & printing it.py
@@ -0,0 +1,6 @@
+#Taking input from user & printing it 
+name = input('name : ')
+age =int(input('age :'))
+price=float(input('price :'))
+
+print('My name is',name,'and I am',age,'years old. the price is',price)
